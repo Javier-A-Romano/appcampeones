3ed301bd0eaefc82fe73a293738c7821ed051ee0
Javier-A-Romano <66748954+Javier-A-Romano@users.noreply.github.com>
2020-06-28 17:14:01 -0300

    build

---
+++ docs/precache-manifest.5f480906aaa7c68eaf5743233f488ea7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9328da6008f6887d85e78134fde2778f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86036e0a3a80c72d81dc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "273004aad5846d808a96",
+    "url": "/static/js/2.12129d53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.12129d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86036e0a3a80c72d81dc",
+    "url": "/static/js/main.d43842fb.chunk.js"
+  },
+  {
+    "revision": "0ca4fcf3faaac76f61d8",
+    "url": "/static/js/runtime-main.74139c71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
